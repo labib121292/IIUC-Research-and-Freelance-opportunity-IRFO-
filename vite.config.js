d48f155f83a1bc3da7bd84463c394e337cf9f06c
Labib Hasan <c231230@ugrad.iiuc.ac.bd>
2025-08-08 23:44:51 +0600

    Add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/IIUC-Research-and-Freelance-opportunity-IRFO-/',
   plugins: [react(), tailwindcss()],
 })
